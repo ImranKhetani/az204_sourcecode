e7caf85f9ca91966609f2adce6fa1060f238ac9a
Imran Khetani <imran_khetani@hotmail.com>
2022-06-12 19:38:15 +0800

    addedfilesfortoday

---
+++ MonitoringApplications_CDN/2. Azure Cache for Redis - .NET/AzureCacheRedis/AzureCacheRedis/Program.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+
+string connectionString = "simplilearnredis.redis.cache.windows.net:6380,password=hsEgOuMS7SYFI24RhdirGKAZaznxGftuqAzCaDDB4So=,ssl=True,abortConnect=False";
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(connectionString);
+
+SetCacheData();
+GetCacheData();
+
+void SetCacheData()
+{
+    IDatabase database=redis.GetDatabase();
+
+    database.StringSet("top:3:courses", "AZ-104,AZ-305,AZ-204");
+
+    Console.WriteLine("Cache data set");
+}
+
+void GetCacheData()
+{
+    IDatabase database = redis.GetDatabase();
+    if (database.KeyExists("top:3:courses"))
+        Console.WriteLine(database.StringGet("top:3:courses"));
+    else
+        Console.WriteLine("key does not exist");
+
+}
